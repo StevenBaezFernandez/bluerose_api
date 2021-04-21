9d1fea9a4291948fe5b688daeeb33cab4ece2bec
Steven <stevenbaezfernandez@gmail.com>
2021-04-21 08:58:40 -0400

    agregando headers

--- api.php
+++ api.php
@@ -1,5 +1,9 @@
 <?php
     header("Content-Type: application/json");
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
     if( isset($_GET['cat1'], $_GET['cat2'], $_GET['cat3']) ){
         if(!isset($_GET['id'])){
             $id = false;
